66bb2a9c4dbc5d21c339cf3adc3e9d514f027b6d
ultragamerman <65148831+ultragamerman@users.noreply.github.com>
2022-04-21 15:05:45 +0530

    Add files via upload

---
+++ Project86/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDOU0gDXP7bs6KBpN-JJVZXpFMuGTnMUmU",
+    authDomain: "class85-f2ae0.firebaseapp.com",
+    databaseURL: "https://class85-f2ae0-default-rtdb.firebaseio.com",
+    projectId: "class85-f2ae0",
+    storageBucket: "class85-f2ae0.appspot.com",
+    messagingSenderId: "16242991949",
+    appId: "1:16242991949:web:326f0421d6e12ad0590d04"
+  };
